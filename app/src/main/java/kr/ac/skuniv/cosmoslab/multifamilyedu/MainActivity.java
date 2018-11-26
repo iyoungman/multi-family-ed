b4ca4e17cc9913ea90d71b7908edefd951535d32
ParkChunSo <cnsth123@gmail.com>
2018-11-26 17:15:35 +0900

    algorism test

--- app/src/main/java/kr/ac/skuniv/cosmoslab/multifamilyedu/MainActivity.java
+++ app/src/main/java/kr/ac/skuniv/cosmoslab/multifamilyedu/MainActivity.java
@@ -1,13 +1,69 @@
 package kr.ac.skuniv.cosmoslab.multifamilyedu;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import kr.ac.skuniv.cosmoslab.multifamilyedu.controller.AnalysisWaveFormController;
+import kr.ac.skuniv.cosmoslab.multifamilyedu.controller.SettingForAnalysisController;
+import kr.ac.skuniv.cosmoslab.multifamilyedu.model.WaveFormModel;
+
+import static android.media.AudioFormat.ENCODING_PCM_16BIT;
 
 public class MainActivity extends AppCompatActivity {
+    private final String ORIGINAL_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/OriginalWav/구름.wav";
+    private final String RECODE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/RecodeWav/구름.wav";;
+
+    private Button button;public AudioRecord mAudioRecord = null;
+    public AudioTrack mAudioTrack = null;
+    private int mAudioSource = MediaRecorder.AudioSource.MIC;
+    private int mSampleRate = 44100;
+    private int mChannelCount = AudioFormat.CHANNEL_IN_STEREO;
+    private int mAudioFormat = ENCODING_PCM_16BIT;
+    private int mBufferSize = AudioTrack.getMinBufferSize(mSampleRate, mChannelCount, mAudioFormat);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int permissionReadStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionWriteStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionAudio = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if(permissionReadStorage == PackageManager.PERMISSION_DENIED || permissionWriteStorage == PackageManager.PERMISSION_DENIED || permissionAudio == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO}, 2);
+        }else {
+            mAudioRecord = new AudioRecord(mAudioSource, mSampleRate, mChannelCount, mAudioFormat, mBufferSize);
+            mAudioRecord.startRecording();
+        }
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SettingForAnalysisController settingForAnalysisController = new SettingForAnalysisController(ORIGINAL_PATH, RECODE_PATH);
+                settingForAnalysisController.controller();
+                WaveFormModel originalModel = settingForAnalysisController.getmOriginalData();
+                WaveFormModel recodeModel = settingForAnalysisController.getmRecodeData();
+
+                AnalysisWaveFormController analysisWaveFormController = new AnalysisWaveFormController(originalModel, recodeModel);
+                int finalScore = analysisWaveFormController.getFinalScore();
+
+                System.out.println(finalScore);
+            }
+        });
+
+
     }
 }
