de3977800b16b2e601ce493648092e0db02983c5
iyoungman <hsl9546@gmail.com>
2019-01-31 02:27:29 +0900

    dd

--- app/src/main/java/kr/ac/skuniv/cosmoslab/multifamilyedu/view/activity/SelectModeActivity.java
+++ app/src/main/java/kr/ac/skuniv/cosmoslab/multifamilyedu/view/activity/SelectModeActivity.java
@@ -38,7 +38,6 @@ public class SelectModeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_mode);
-
         achievementSharedPreferences = getSharedPreferences("achievement", MODE_PRIVATE);
 
         startBtn = findViewById(R.id.start_button);
